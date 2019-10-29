b20c5df9e0b80d98ab46236120feb6ca49bac178
Wajih Katrou <wajih.katrou@ensi-uma.tn>
2019-10-29 14:58:31 +0100

    Creating the database tables

--- domain_specific_language/cities/models.py
+++ domain_specific_language/cities/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+
+	code = models.IntegerField()
+	name = models.CharField(max_length = 100)
+	population = models.IntegerField()
+	average_age = models.FloatField()
+	distr_code = models.IntegerField()
+	dept_code = models.IntegerField()
+	region = models.ForeignKey("Region", to_field="region_code", db_column="region_code", related_name = "city_region", on_delete=models.CASCADE)
+	
+	class Meta:
+		verbose_name = "Town"
+
+
+class Region(models.Model):
+
+	region_code = models.IntegerField(unique=True)
+	region_name = models.CharField(max_length = 100)
+	
+	class Meta:
+		verbose_name = "Region"
